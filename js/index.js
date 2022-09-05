cdd5df02c2d6b117ea8211f1d4b35d8005f01b62
Sekaia <gillotte.bm@gmail.com>
2022-09-05 08:19:24 -0500

    created first object

--- js/index.js
+++ js/index.js
@@ -1,30 +1,7 @@
-function displayArray(arr){
-    let html = ""
-    for (let i = 0; i < arr.length; i++){
-        html += `<li>${arr[i]}</li>`;
-    }
-    return html;
+const wisp = {
+    name: 'Aero',
+    job: 'Knight',
+    age: 23,
+    type: 'Vim',
+    skills: ['swordsman', 'blacksmithing', 'adventurer']
 }
-
-const main = document.querySelector("main");
-
-
-let wispTypes = [
-    "Moxie",
-    "Vim",
-    "Chum",
-]
-
-let jobTypes = [
-    "Librarian",
-    "Guard",
-    "Scientist",
-    "Doctor",
-    "Teacher",
-    "Trainer",
-    "Chef",
-    "Merchant"
-]
-
-main.innerHTML += `<ol>${displayArray(wispTypes)}</ol>`
-main.innerHTML += `<ol>${displayArray(jobTypes)}</ol>`
