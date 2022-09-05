129e20976c8f255f36a69cbe76a2303d5d097c72
Sekaia <gillotte.bm@gmail.com>
2022-09-05 08:46:59 -0500

    added writing function and loop to display object

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
+function writeToMain(html){
+    document.querySelector("main").innerHTML += html;
+}
+
 const wisp = {
     name: 'Aero',
     job: 'Knight',
     age: 23,
     type: 'Vim',
-    skills: ['swordsman', 'blacksmithing', 'adventurer']
+    skills: ['swordsman', 'blacksmith', 'adventurer']
+}
+
+for (let key in wisp){
+    writeToMain(`<p>${key}: ${wisp[key]}</p>`);
 }
